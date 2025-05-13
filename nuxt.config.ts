147ef8338f0cf6a375987315991d8ee600d52466
jwansoo <jwansoo5197@gmail.com>
2025-05-13 21:11:49 +0900

    Make some better changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
 });
